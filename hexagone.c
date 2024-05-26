f3a492596625eeee4b55a4bee62d0182c8e8d73e
ABDELMOULAZIED <abdelmoula.zd@gmail.com>
2024-05-26 22:10:30 +0200

    Update hexagone.c

--- hexagone.c
+++ hexagone.c
@@ -7,69 +7,94 @@
 #define COLUMN 9
 #define NOM 50
 
-// Cette fonction permet de créer un élément de la structure hexagone.
-Hexagone *createHexagone(int fish, int x, int y, int z) {
+Hexagone *createHexagone(int fish, int x, int y, int z,int a) {
     Hexagone *hexa = malloc(sizeof(Hexagone));
     if (hexa == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire pour l'hexagone.\n");
+        fprintf(stderr, "Memory allocation error for hexagon.\n");
         exit(EXIT_FAILURE);
     }
+    // Assign the values to the hexagone
     hexa->x = x;
     hexa->y = y;
     hexa->fish = fish;
-    hexa->active = 0;
-    hexa->joueur = 0;
-    hexa->penguin = z;
+    hexa->active = a;
+    hexa->player = 0;
+    hexa->animal = z;
     return hexa;
 }
 
-// Cette fonction permet de créer un tableau d'éléments de la structure hexagone.
 Hexagone **createGrid(int n, int m, int nbj) {
     Hexagone **grid = malloc(n * sizeof(Hexagone *));
     if (grid == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire pour la grille.\n");
+        fprintf(stderr, "Memory allocation error for grid.\n");
         exit(EXIT_FAILURE);
     }
+    // Create the grid
     for (int i = 0; i < n; i++) {
         grid[i] = malloc(m * sizeof(Hexagone));
         if (grid[i] == NULL) {
-            fprintf(stderr, "Erreur d'allocation mémoire pour la grille.\n");
+            fprintf(stderr, "Memory allocation error for grid.\n");
             for (int k = 0; k < i; k++) {
                 free(grid[k]);
             }
             free(grid);
             exit(EXIT_FAILURE);
         }
+        // Initialize the grid with hexagones
         int tab[3];
         switch(nbj)
             {
                 case 2:
-                    tab[0] = (rand() % 77) + 8;
-                
+                    tab[0] = (rand() % 77-8) + 8;
+                    tab[1] = (rand() % 77-tab[0]) ;
+                    tab[2] = 77 - tab[0] - tab[1];
+                    break;
+                case 3:
+                    tab[0] = (rand() % 77-9) + 9;
+                    tab[1] = (rand() % 77-tab[0]) ;
+                    tab[2] = 77 - tab[0] - tab[1];
+                    break;
+                case 4:
+                    tab[0] = (rand() % 77-8) + 8;
+                    tab[1] = (rand() % 77-tab[0]) ;
+                    tab[2] = 77 - tab[0] - tab[1];
+                    break;
+                case 5:
+                    tab[0] = (rand() % 77-5) + 5;
+                    tab[1] = (rand() % 77-tab[0]) ;
+                    tab[2] = 77 - tab[0] - tab[1];
+                    break;
+                case 6:
+                    tab[0] = (rand() % 77-6) + 6;
+                    tab[1] = (rand() % 77-tab[0]) ;
+                    tab[2] = 77 - tab[0] - tab[1];
+                    break; 
             }
+
+        // Assign a random number of fish to each hexagone
         for (int j = 0; j < m; j++) {
-            int fish = (rand() % 3) + 1;
-            grid[i][j] = *createHexagone(fish, i, j, 0);
+            int fish;
+            do{
+                fish = (rand() % 3) + 1;
+            }while(tab[fish-1]==0);
+            if(i == 8 && j%2 !=0){
+                grid[i][j] = *createHexagone(0, i, j, -1,-1);
+            }
+            else{
+                grid[i][j] = *createHexagone(fish, i, j, -1,0);
+            }
+            
         }
     }
     return grid;
 }
 
-Hexagone *recherche_hexa(Hexagone **grid, int x, int y) {
-    if (x < 0 || x >= ROW || y < 0 || y >= COLUMN) {
-        return NULL;
-    }
-    return &grid[x][y];
-}
-
-// Cette fonction va afficher les infos contenues dans la grille d'hexagones.
 void printInformationGrid(Hexagone **grid, int n, int m) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            printf("nombre de poissons : %d, abscisse : %d, ordonnée : %d\n",
+            printf("number of fish : %d, abscissa : %d, ordinate : %d\n",
                    grid[i][j].fish, grid[i][j].x, grid[i][j].y);
         }
     }
 }
 
-// Cette procédure va permettre d'afficher la grille hexagonale avec les poissons dedans en fonction des infos de chaque hexagone.
