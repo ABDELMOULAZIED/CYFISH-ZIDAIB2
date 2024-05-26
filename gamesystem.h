1f06f5aed74e4af89300cf94cc4e3875a9befa4f
ABDELMOULAZIED <abdelmoula.zd@gmail.com>
2024-05-26 22:10:12 +0200

    Update gamesystem.h

--- gamesystem.h
+++ gamesystem.h
@@ -1,18 +1,21 @@
 #ifndef GAMESYSTEM_H
-#include "hexagone.h"
-#include "joueur.h"
-#define GAMESYSTEM_H
-#define TAB_DEPLACEMENT_INIT                                                   \
-  { "Haut", "Bas", "Bas Gauche", "Bas Droite", "Haut Gauche", "Haut Droite" }
-void printHexa(int penguin);
-void printHexa2(int penguin);
-void printGrid(Hexagone **grid, int n, int m);
-Joueur *beginning(int nbj, Hexagone **grid);
-void printGame(Joueur *tab, int nbj, Hexagone **grid);
-void deplacement_joueur(Pingouin p, int *new_x, int *new_y, Hexagone **grid);
-void tour(Joueur *tab, int nbj, Hexagone **grid, int *new_x, int *new_y);
-void choose_position_depart(Joueur *j, Hexagone **grid, int nbj);
-void printGame(Joueur *tab, int nbj, Hexagone **grid);
-void pingouin_bloquer(Hexagone **grid,  Pingouin p );
-int verif_joueur_bloquer(Hexagone **grid, Joueur j);
+  #include "hexagone.h"
+  #include "joueur.h"
+  #define GAMESYSTEM_H
+  #define TAB_DEPLACEMENT_INIT { "Up", "down", "down left", "down right", "up left", "up right" }
+
+  void printHexa(int animal); // print the emoji of the animal
+  void printGrid(Hexagone **grid, int n, int m); // print the hexa grid
+  Player *beginning(int nbj, Hexagone **grid); // create the table of players
+  int case_sink(Hexagone hexa); //check if a hexagone has sunk
+  int verifMooves(Hexagone **grid, int x1, int y1, int x2, int y2);//check if a moove is valid.
+  int* testMooves(Animal p, Hexagone** grid, int direction_deplacement);
+  void start_auto(Hexagone **grid,Player *tab,int nbj);
+  void printGame(Player *tab, int nbj, Hexagone **grid);// print the grid and players informations
+  void moove_player(Animal p, int *new_x, int *new_y, Hexagone **grid); // ask the player to move his animal
+  int turn(Player *tab, int nbj, Hexagone **grid, int *new_x, int *new_y);// the turn of the player
+  void choose_position_start(Player *j, Hexagone **grid, int nbj, int i);// ask the player to choose the position of his animal
+  void choose_position_start2(Player *j, Hexagone **grid, int nbj, int i);
+  int animal_blocked(Hexagone **grid,  Animal p );// check if a pingouin is blocked
+  int player_blocked(Hexagone **grid, Player j);// check if a player is blocked
 #endif
