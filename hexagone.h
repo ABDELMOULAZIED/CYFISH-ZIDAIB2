f4308fd3586f388b402dabd421338c7b1cdffd00
ABDELMOULAZIED <abdelmoula.zd@gmail.com>
2024-05-26 22:10:45 +0200

    Update hexagone.h

--- hexagone.h
+++ hexagone.h
@@ -5,14 +5,13 @@ typedef struct {
     int x, y;
     int fish;
     int active;
-    int joueur;
-    int penguin;
+    int player;
+    int animal;
 } Hexagone;
 
-Hexagone *createHexagone(int fish, int x, int y, int z);
-Hexagone **createGrid(int n, int m, int nbj);
-Hexagone *recherche_hexa(Hexagone **grid, int x, int y);
-void printInformationGrid(Hexagone **grid, int n, int m);
+Hexagone *createHexagone(int fish, int x, int y, int z, int a);//this function creates an element of the structure hexagone.
+Hexagone **createGrid(int n, int m, int nbj);//this function creates an array of elements of the structure hexagone.
+void printInformationGrid(Hexagone **grid, int n, int m);//this function allows you to print the information of the hexagone.
 
 
 #endif
